15fd6b9850f1984f74f37e29a03a9c2a089a1cdb
Andrew Menendez <menendez.andrew@gmail.com>
2017-08-27 16:23:24 -0700

    cant change source

--- src/components/IframePanel.vue
+++ src/components/IframePanel.vue
@@ -3,19 +3,22 @@
     <h1 class="ui center aligned header"> iFrame goes here</h1>
     <div class="ui container">
       <div class="ui segments">
-        <div class="ui raised segment">
-          <div class='ui two buttons'>
-              <button class='ui basic blue button'>
+        <div class="ui segment">
+          <div class="ui buttons">
+              <button class='ui  blue button' v-on:click="changeUrl('https://developer.mozilla.org/en-US/docs/Web/HTML/Element/iframe')">
                 SalesForce
               </button>
-              <button class='ui basic yellow button'>
+              <div class="or"></div>
+              <button class='ui  yellow button'>
                 Insights
               </button>
           </div>
           <div class="ui segment">
 
             <!-- <div class="ui embed" data-url="https://en.wikipedia.org/wiki/Benjamin_Franklin" data-placeholder="./assets/logo.pngg"></div> -->
-            <iframe id="main-iframe" src="https://en.wikipedia.org/wiki/Benjamin_Franklin"></iframe>
+            <p ref="test"></p>
+            <p>{{frameurl}}</p>
+            <iframe v-if="loaded" ref="mainIframe" id="main-iframe" v-bind:src="frameurl" ></iframe>
           </div>
         </div>
 
@@ -25,7 +28,31 @@
 </template>
 
 <script type="text/javascript">
-export default {}
+export default {
+  data () {
+    return {
+      frameurl: 'https://en.wikipedia.org/wiki/Benjamin_Franklin',
+      loaded: false
+      // iframe: {
+      //   src: window.location.href,
+      //   style: null,
+      //   wrapperStyle: null
+      // }
+    }
+  },
+  methods: {
+    changeUrl (url) {
+      console.log('url is ', url)
+      console.log(this.$refs)
+      this.$refs.test.innerText = url
+      this.frameurl = url
+    }
+  },
+  mounted () {
+    // let editor = this.$refs.editor;
+    this.loaded = true
+  }
+}
 </script>
 
 <style>
--- src/components/TablePanel.vue
+++ src/components/TablePanel.vue
@@ -1,6 +1,87 @@
 <template>
   <div id="tablepanel">
     <h1 class="ui center aligned header"> Table goes here</h1>
+    <div class="ui container">
+      <div class="ui segments">
+        <div class="ui segment">
+          <div class="ui buttons">
+              <button class='ui basic blue button'>
+                Do something
+              </button>
+
+              <button class='ui basic teal button'>
+                Something else
+              </button>
+          </div>
+        </div>
+        <div class="ui segment">
+          <table class="ui celled table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Status</th>
+                <th>Notes</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>No Name Specified</td>
+                <td>Unknown</td>
+                <td class="negative">None</td>
+              </tr>
+              <tr class="positive">
+                <td>Jimmy</td>
+                <td><i class="icon checkmark"></i> Approved</td>
+                <td>None</td>
+              </tr>
+              <tr>
+                <td>Jamie</td>
+                <td>Unknown</td>
+                <td class="positive"><i class="icon close"></i> Requires call</td>
+              </tr>
+              <tr class="negative">
+                <td>Jill</td>
+                <td>Unknown</td>
+                <td>None</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="ui segment">
+          <table class="ui celled table">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Status</th>
+                <th>Notes</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>No Name Specified</td>
+                <td>Unknown</td>
+                <td class="negative">None</td>
+              </tr>
+              <tr class="positive">
+                <td>Jimmy</td>
+                <td><i class="icon checkmark"></i> Approved</td>
+                <td>None</td>
+              </tr>
+              <tr>
+                <td>Jamie</td>
+                <td>Unknown</td>
+                <td class="positive"><i class="icon close"></i> Requires call</td>
+              </tr>
+              <tr class="negative">
+                <td>Jill</td>
+                <td>Unknown</td>
+                <td>None</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
